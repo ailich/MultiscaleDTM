99aebc83779c546034a0a5e7cc57c0a31dd806a1
Alex Ilich <ailich@usf.edu>
2021-10-25 12:15:27 -0700

    Remove spatialEco dependency

--- R/VRM.R
+++ R/VRM.R
@@ -1,25 +1,41 @@
 #' Implementation of the Sappington et al., (2007) vector ruggedness measure
 #'
-#' Implementation of the Sappington et al., (2007) vector ruggedness measure. This is a simple wrapper around the vrm function from spatialEco by Jeffrey S Evans, to make the syntax consistent with the rest of the MultiscaleDEM package, and is included in this package since VRM is also a rugosity measure that corrects for slope. If you use this function, please cite the spatialEco package.
+#' Implementation of the Sappington et al., (2007) vector ruggedness measure, modified from Evans (2021). 
 #' @param r DEM as a raster layer
 #' @param w A vector of length 2 specifying the dimensions of the rectangular window to use where the first number is the number of rows and the second number is the number of columns. Window size must be an odd number.
 #' @param include_scale logical indicating whether to append window size to the layer names (default = FALSE)
 #' @return a RasterLayer
 #' @import raster
-#' @importFrom spatialEco vrm
 #' @references
+#' Evans JS (2021). spatialEco. R package version 1.3-6, https://github.com/jeffreyevans/spatialEco.
+#' 
 #' Sappington, J.M., Longshore, K.M., Thompson, D.B., 2007. Quantifying Landscape Ruggedness for Animal Habitat Analysis: A Case Study Using Bighorn Sheep in the Mojave Desert. The Journal of Wildlife Management 71, 1419-1426. https://doi.org/10.2193/2005-723
 #' @export
 
 VRM<- function(r, w, include_scale=FALSE){
+  if (class(r)[1] != "RasterLayer") 
+    stop("r must be a raster object")
+  if(length(w) > 2){
+    stop( "Specified window exceeds 2 dimensions")}
   if(length(w)==1){
     w<- rep(w,2)}
-  out<- spatialEco::vrm(x=r, s=w)
+  scale.factor <- round(w[1] * w[2], 0)
+  sa <- raster::terrain(r, opt=c("slope", "aspect"), unit="radians", 
+                        neighbors=8) 					
+  sin.slp <- raster::calc(sa[["slope"]], fun=sin)                 # xyRaster 
+  cos.slp <- raster::calc(sa[["slope"]], fun=cos)                 # zRaster 
+  sin.asp <- raster::calc(sa[["aspect"]], fun=sin) * sin.slp      # yRaster
+  cos.asp <- raster::calc(sa[["aspect"]], fun=cos) * sin.slp      # xRaster  
+  f = matrix(1,w[1],w[2]) 
+  x.sum <- raster::focal(sin.asp, w = f, fun=sum) 
+  y.sum <- raster::focal(cos.asp, w = f, fun=sum) 
+  z.sum <- raster::focal(cos.slp, w = f, fun=sum) 
+  vrm.fun <- function(x, y, z) { 
+    sqrt( (x^2) + (y^2) + (z^2) ) 
+  }
+  res_vect <- raster::overlay(x.sum, y.sum, z.sum, fun=vrm.fun) #resultant vector
+  out<- 1 - (res_vect / scale.factor)
   names(out)<- "vrm"
   if(include_scale){names(out)<- paste0(names(out), "_", w[1],"x", w[2])} #Add scale to layer names
   return(out)
-}
-
-
-
-
+  }
